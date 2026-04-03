4f5154d516a61ed961d728b24cf366f8a2a8da61
Muhammad-Haris-Abdull-Bari <M.haris.abdul.bari01@gmail.com>
2026-04-04 02:09:57 +0500

    Fix gsap-trial module declarations

--- src/types/gsap-trial.d.ts
+++ src/types/gsap-trial.d.ts
@@ -1,1 +1,17 @@
-declare module 'gsap-trial/gsplittext' { const SplitText: any; export default SplitText; export { SplitText }; }
+declare module 'gsap-trial/ScrollSmoother' {
+  const ScrollSmoother: any;
+  export default ScrollSmoother;
+  export { ScrollSmoother };
+}
+
+declare module 'gsap-trial/SplitText' {
+  const SplitText: any;
+  export default SplitText;
+  export { SplitText };
+}
+
+declare module 'gsap-trial/*' {
+  const value: any;
+  export default value;
+  export { value };
+}
